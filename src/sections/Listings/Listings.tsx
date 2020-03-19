aa68b3cd01f729d5d5316810cab5b7ae020869b8
cl0udsurfer <datakobs1@gmail.com>
2020-03-19 14:36:05 +0100

    integrated apollo client

--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { server } from '../../lib/api/';
+import { useQuery, useMutation } from 'react-apollo';
+import { gql } from 'apollo-boost';
+import { Listings as ListingsData } from './__generated__/Listings';
 import {
-  ListingsData,
-  DeleteListingData,
+  DeleteListing as DeleteListingData,
   DeleteListingVariables
-} from './types';
+} from './__generated__/DeleteListing';
 
-const LISTINGS = `
+const LISTINGS = gql`
   query Listings {
     listings {
       id
@@ -22,12 +23,12 @@ const LISTINGS = `
   }
 `;
 
-const DELETE_LISTING = `
- mutation DeleteListing($id: ID!) {
-   deleteListing(id: $id) {
-     id
-   }
- }
+const DELETE_LISTING = gql`
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
 `;
 
 interface Props {
@@ -35,28 +36,59 @@ interface Props {
 }
 
 export const Listings = ({ title }: Props) => {
-  const fetchListings = async () => {
-    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
-    console.log(data);
+  const { data, loading, error, refetch } = useQuery<ListingsData>(LISTINGS);
+
+  const [
+    deleteListing,
+    { loading: deleteListingLoading, error: deleteListingError }
+  ] = useMutation<DeleteListingData, DeleteListingVariables>(DELETE_LISTING);
+
+  const handleDeleteListing = async (id: string) => {
+    await deleteListing({ variables: { id } });
+    refetch();
   };
 
-  const deleteListing = async () => {
-    const { data } = await server.fetch<
-      DeleteListingData,
-      DeleteListingVariables
-    >({
-      query: DELETE_LISTING,
-      variable: {
-        id: ''
-      }
-    });
-  };
+  const listings = data ? data.listings : null;
+
+  const listingsList = listings ? (
+    <ul>
+      {listings.map(listing => {
+        return (
+          <li key={listing.id}>
+            {listing.title}{' '}
+            <button onClick={() => handleDeleteListing(listing.id)}>
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  ) : null;
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>Uh oh! Something went wrong - please try again later :(</h2>;
+  }
+
+  const deleteListingLoadingMessage = deleteListingLoading ? (
+    <h4>Deletion in progress...</h4>
+  ) : null;
+
+  const deleteListingErrorMessage = deleteListingError ? (
+    <h4>
+      Uh oh! Something went wrong with deleting :(. Please try again soon.
+    </h4>
+  ) : null;
 
   return (
     <div>
       <h2>{title}</h2>
-      <button onClick={fetchListings}>Query Listings</button>
-      <button onClick={deleteListing}>Delete Listing</button>
+      {listingsList}
+      {deleteListingLoadingMessage}
+      {deleteListingErrorMessage}
     </div>
   );
 };
